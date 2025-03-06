7db2c4c81d1e48cce10f6c7ede34ab161560f625
talys <talysmarques2017@gmail.com>
2025-03-05 22:41:31 -0300

    primeiros testes com funções

--- scripts.js
+++ scripts.js
@@ -4,12 +4,41 @@ function MyKO() {
     self.user = {
         name: ko.observable('' || 'você'),
         lastName: ko.observable(''),
-        age: ko.observable('' || '000'),
-        goal: ko.observable('' || '000'),
-        currentGoal: ko.observable('000')
+        age: ko.observable(''),
+        goal: ko.observable(0),
+        currentGoal: ko.observable(0)
     }
 
+    self.user.goalText = ko.computed(() => self.user.goal().toString().padStart(3, '0'));
+    self.user.currentGoalText = ko.computed(() => self.user.currentGoal().toString().padStart(3, '0'));
+
     // fim das informações do usuario
+
+    self.actualPage = ko.observable(10)
+
+    self.openThisPage = function (page) {
+        self.actualPage(page)
+    }
+
+
+
+
+
+
+    self.teste = function () {
+        self.user.currentGoal(self.user.currentGoal() + 1)
+    }
+
+    self.tempName = ko.observable('')
+    self.saveName = function () {
+        if (self.tempName().length > 0) {
+            if (self.tempName() == 'gil' || self.tempName() == 'Gil' || self.tempName() == 'Gilvane' || self.tempName() == 'gilvane') {
+                self.tempName('Nojento')
+            }
+            self.user.name(self.tempName())
+        }
+        self.tempName('')
+    }
 }
 
 ko.applyBindings(new MyKO());
